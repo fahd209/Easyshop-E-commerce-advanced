811e04df672068ff38ec59774010d3b6c88f1355
Fahd <fahds2005@icloud.com>
2024-06-28 16:21:51 -0700

    first push

---
+++ easyshop-client-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
